7ebe106147ae21000467e02d53d64bb5949e10a7
Mukhammad Kagir <kagirxadjiev18@gmail.com>
2022-08-16 19:01:43 +0300

    js

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("Hello,World")
+// console.log(2+3)
+// console.log(10-2);
+// console.log(2*3);
+// console.log(10/5);
+// console.log(2**3);
+// console.log((2+3) + (10-2) + (2*3) + (10/5) + (2**3));
+
+// let, const
+    
+
+
+// console.log(m1,m2,m3,m4,m5);
+// let m1 = 19
+// let m2 = 2002
+// let m3 = 22
+// let m4 = 129
+// let m5 = 2249
+
+
+let nameAge = ["Maga", 19, "Bulat", 18, "Turpal", 18, "Zubayra", 19, "Hojik", 17]
+console.log("Имя:", nameAge[0], ",", "Возраст;", nameAge[1]); 
+console.log("Имя:", nameAge[2], ",", "Возраст;", nameAge[3]); 
+console.log("Имя:", nameAge[4], ",", "Возраст;", nameAge[5]); 
+console.log("Имя:", nameAge[6], ",", "Возраст;", nameAge[7]); 
+console.log("Имя:", nameAge[8], ",", "Возраст;", nameAge[9]); 
